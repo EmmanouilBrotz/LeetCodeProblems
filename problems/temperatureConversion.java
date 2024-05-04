1526d31cf16813a85449735939b8e161e42df395
EmmanouilBrotz <ebrotzakis@nyc.gr>
2024-05-04 17:53:50 +0300

    solved problem: 2469. convert the temperature

---
+++ problems/temperatureConversion.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class temperatureConversion {
+    public static void main(String[] args) {
+        System.out.println("Please input temperature for conversion to Fahrenheit and Kelvin.");
+        Scanner sc = new Scanner(System.in);
+        double temp = Double.parseDouble(sc.nextLine());
+        System.out.println(Arrays.toString(convertTemperature(temp)));
+
+    }
+    public static double[] convertTemperature(double celsius){
+        return new double[] {celsius + 273.15, (celsius * 1.80) + 32.00};
+    }
+}
